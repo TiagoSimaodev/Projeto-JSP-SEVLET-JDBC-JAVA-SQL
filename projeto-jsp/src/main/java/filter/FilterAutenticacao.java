c3f7f1ce23d4c4816065853ecafe16521d309327
Tiago Simao <tiagosimaorodri123@gmail.com>
2025-01-19 03:35:26 -0300

    Incluído no pom.xml a dependencia do postgresql-maven.

--- projeto-jsp/src/main/java/filter/FilterAutenticacao.java
+++ projeto-jsp/src/main/java/filter/FilterAutenticacao.java
@@ -13,48 +13,70 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@WebFilter(urlPatterns = {"/principal/*"}) // Interceptas todas as requisições que vierem do projeto ou mapeamento
+import connection.SingleConnectionBanco;
+
+@WebFilter(urlPatterns = { "/principal/*" }) // Interceptas todas as requisições que vierem do projeto ou mapeamento
 public class FilterAutenticacao extends HttpFilter implements Filter {
-       
-    
-    public FilterAutenticacao() {
-    }
-    
-    // encerra os processo quando o servidor é parado
-    // Mataria os processos de conexão com o banco
+
+	private static Connection connection;
+
+	public FilterAutenticacao() {
+	}
+
+	// encerra os processo quando o servidor é parado
+	// Mataria os processos de conexão com o banco
 	public void destroy() {
-	}
-
-	
-	//Intercepta as requisições e as resposta no sistema
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpSession session = req.getSession();
-		
-		String usuarioLogado = (String) session.getAttribute("usuario");
-		
-		String urlParaAutenticar = req.getServletPath(); // url que está sendo acessada
-		
-		// Validar se está logado, se não redireciona a tela de login
-		if(usuarioLogado == null &&
-				 !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) { // não está logado
-			RequestDispatcher redireciona =request.getRequestDispatcher("/index.jsp?url="+ urlParaAutenticar);
-			request.setAttribute("msg", "por favor realize o login!");
-			redireciona.forward(request, response);
-			return; //para a execução e redireciona para o login
-			
-		}else {
-			chain.doFilter(request, response);
-			
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
 	}
 
-	
+	// Intercepta as requisições e as resposta no sistema
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		try {
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+
+			String usuarioLogado = (String) session.getAttribute("usuario");
+
+			String urlParaAutenticar = req.getServletPath(); // url que está sendo acessada
+
+			// Validar se está logado, se não redireciona a tela de login
+			if (usuarioLogado == null && !urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) { // não está
+																											// logado
+				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+				request.setAttribute("msg", "por favor realize o login!");
+				redireciona.forward(request, response);
+				return; // para a execução e redireciona para o login
+
+			} else {
+				chain.doFilter(request, response);
+
+			}
+			
+			connection.commit();//deu tudo certo, então comita as alterações no banco de dados.
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
 	// Inicia os processos ou recursos quando o servidor sobe o projeto
-	//	iniciar a conexão com o banco
+	// iniciar a conexão com o banco
 	public void init(FilterConfig fConfig) throws ServletException {
+		connection = SingleConnectionBanco.getConnection();
 	}
 
 }
