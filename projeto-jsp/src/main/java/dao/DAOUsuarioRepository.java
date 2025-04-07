78fd2cca76052192b222083ef3ccd457acef9e6b
Tiago Simao <tiagosimaorodri123@gmail.com>
2025-04-07 00:11:19 -0300

    criado o modelTelefone e criado os atributos da tabela de telefone e seus get e set e hashCode. Foi feita também o DaoTelefoneRepository e feito o crud do telefone,com os métodos de gravaTelefone, deleteTelefone, e a listaTelefone. E feita atualização no daoUsuarioRepostory onde foi criado a consulta somente por ID.

--- projeto-jsp/src/main/java/dao/DAOUsuarioRepository.java
+++ projeto-jsp/src/main/java/dao/DAOUsuarioRepository.java
@@ -390,6 +390,43 @@ public ModelLogin consultaUsuario(String login) throws Exception {
 		return modelLogin;
 	}
 	
+	
+public ModelLogin consultaUsuarioID(Long id) throws Exception {
+		
+		ModelLogin modelLogin = new ModelLogin(); 
+		
+		String sql = "select * from model_login where id = ? and useradmin is false";
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setLong(1, id);
+
+		ResultSet resultado = statement.executeQuery();
+		
+		while(resultado.next()) {  // se tem resultaado 
+			modelLogin.setId(resultado.getLong("id"));
+			modelLogin.setEmail(resultado.getString("email"));
+			modelLogin.setLogin(resultado.getString("login"));
+			modelLogin.setSenha(resultado.getString("senha"));
+			modelLogin.setNome(resultado.getString("nome"));
+			modelLogin.setPerfil(resultado.getString("perfil"));
+			modelLogin.setSexo(resultado.getString("sexo"));
+			modelLogin.setFotouser(resultado.getString("fotouser"));
+			modelLogin.setExtensaofotouser(resultado.getString("Extensaofotouser"));
+
+			modelLogin.setCep(resultado.getString("cep"));
+			modelLogin.setLogradouro(resultado.getString("logradouro"));
+			modelLogin.setBairro(resultado.getString("bairro"));
+			modelLogin.setLocalidade(resultado.getString("localidade"));
+			modelLogin.setUf(resultado.getString("uf"));
+			modelLogin.setNumero(resultado.getString("numero"));
+			
+		}
+	
+		return modelLogin;
+	}
+	
+	
+	
+	
 public ModelLogin consultaUsuarioID(String id, Long userLogado) throws Exception {
 		
 		ModelLogin modelLogin = new ModelLogin(); 
